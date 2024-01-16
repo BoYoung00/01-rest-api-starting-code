049bb12e418c1e4e3345890b2970fee903e4fb73
BoYoung00 <qhdud20@naver.com>
2024-01-16 15:58:02 +0900

    24/01/10 REST API 공부 (리액트 연동용), 시큐리티

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -3,6 +3,8 @@ package com.in28minutes.rest.webservices.restfulwebservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,8 +22,8 @@ public class RestfulWebServicesApplication {
 				// 특정한 패턴에 대해 크로스 오리진 리퀘스트 처리
 				registry.addMapping("/**")
 						.allowedOrigins("http://localhost:3000") // 허용할 출처를 명시 (필요에 따라 수정)
-						.allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드를 명시
-						.allowedHeaders("Origin", "Content-Type", "Accept"); // 허용할 헤더를 명시
+						.allowedMethods("*") // 허용할 HTTP 메서드를 명시
+						.allowedHeaders("*"); // 허용할 헤더를 명시
 			}
 		};
 	}
