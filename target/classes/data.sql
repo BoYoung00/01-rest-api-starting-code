e5aeadc499bc3688b52337404d6dfaf2995060d7
BoYoung00 <qhdud20@naver.com>
2024-01-22 09:45:00 +0900

    24/01/18 REST API 공부 (리액트 연동용), JWT, H2 -> mysql로 변경

---
+++ target/classes/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO todo (id, description, done, target_date, username)
+VALUES (10001, 'aaaaaaaaaaaaaa', false, CURRENT_DATE(), 'kim');
+
+INSERT INTO todo (id, description, done, target_date, username)
+VALUES (10002, 'bbbbbbbbbbbbbbb', false, CURRENT_DATE(), 'kim');
+
+INSERT INTO todo (id, description, done, target_date, username)
+VALUES (10003, 'ccccccccccccccc', false, CURRENT_DATE(), 'kim');
